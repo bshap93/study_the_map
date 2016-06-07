47e22961772fbea215b811dfefa61f07b42ddad8
bshap93 <benihana858@gmail.com>
2016-06-07 14:25:05 -0500

    Change gemfile

--- spec/study_the_map_spec.rb
+++ spec/study_the_map_spec.rb
@@ -4,8 +4,4 @@ describe StudyTheMap do
   it 'has a version number' do
     expect(StudyTheMap::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
