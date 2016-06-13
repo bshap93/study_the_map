d5bb46438779033c4414b39134ead61441141c4e
bshap93 <benihana858@gmail.com>
2016-06-13 10:57:26 -0500

    Add tests for lookupids.rb

--- spec/lookupids_spec.rb
+++ spec/lookupids_spec.rb
@@ -4,9 +4,26 @@ describe LookupIDS do
   describe "self.index" do
     context "any" do
       it "returns fixtures/index" do
-        binding.pry
-        expect(LookupIDS.index).to eql(0)
+        index = Nokogiri::XML(open("./lib/fixtures/index.xml"))
+        expect(LookupIDS.index.text).to eq(index.text)
       end
     end
   end
+
+  describe ".find_region_id" do
+    context "given Utah" do
+      it "returns '319'" do
+        expect(LookupIDS.find_region_id("Utah")).to eq("319")
+      end
+    end
+  end
+
+  describe ".find_ski_area_id" do
+    context "given Snowbird Ski and Summer Resort" do
+      it "returns '226'" do
+        expect(LookupIDS.find_ski_area_id("Snowbird Ski and Summer Resort")).to eq("226")
+      end
+    end
+  end
+
 end
